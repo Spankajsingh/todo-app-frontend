8dad83346f559f8480d7f5b11c831a08a5781546
Spankajsingh <singhpankajmmm@gmail.com>
2021-01-16 19:39:19 +0530

    added some feature

--- src/component/todo/AuthenticationService.js
+++ src/component/todo/AuthenticationService.js
@@ -13,6 +13,13 @@ class AuthenticationService {
         if(user===null) return false
         return true;
     }
+
+    getLoggedInUser() {
+        let user = sessionStorage.getItem('authenticatedUser');
+        if(user===null) return ''
+        return user
+    }
+
 }
 
 export default new AuthenticationService()
--- src/component/todo/ListTodosComponent.jsx
+++ src/component/todo/ListTodosComponent.jsx
@@ -1,22 +1,73 @@
 import React, {Component} from 'react'
+import TodoDataService from '../../api1/todoService/TodoDataService.js'
+import AuthenticationService from './AuthenticationService'
 
 class ListTodosComponent extends Component {
 
     constructor(props) {
+        //console.log('constructor')
         super(props)
         this.state = {
             todos : [
-                {id: 1, description: 'Learn React', done: false, targetDate: new Date()},
-                {id: 2, description: 'Learn SpringBoot', done: false, targetDate: new Date()},
-                {id: 3, description: 'Microservices', done: false, targetDate: new Date()},
-            ]
+                // {id: 1, description: 'Learn React', done: false, targetDate: new Date()},
+                // {id: 2, description: 'Learn SpringBoot', done: false, targetDate: new Date()},
+                // {id: 3, description: 'Microservices', done: false, targetDate: new Date()},
+            ],
+            message: null
         }
+        this.deleteTodoClicked = this.deleteTodoClicked.bind(this)
+        this.refreshTodos = this.refreshTodos.bind(this)
+    }
+
+    componentWillUnmount() {
+        //console.log('componentWillUnmount')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        //console.log('shouldComponentUpdate')
+        //console.log(nextProps)
+        //console.log(nextState)
+        return true
+    }
+
+    componentDidMount() {
+        //console.log('componentDidMount')
+        this.refreshTodos();
+    }
+
+    refreshTodos() {
+        let username = AuthenticationService.getLoggedInUser();
+        TodoDataService.retrieveAllTodos(username)
+        .then(
+            response => {
+                //console.log(response);
+                this.setState({todos: response.data})
+            }
+        )
+    }
+
+    deleteTodoClicked(id) {
+        let username = AuthenticationService.getLoggedInUser();
+        //console.log(id + " " + username);
+        TodoDataService.deleteTodo(username, id)
+        .then(response => 
+            {
+                this.setState({message : `Delete of todo ${id} Successful`})
+                this.refreshTodos()
+            })
+    }
+
+    updateTodoClicked(id) {
+        //console.log("'update " + id)
+        this.props.history.push(`/todos/${id}`)
     }
 
     render() {
+        //console.log('render')
         return (
             <div>
                 <h1>List Todos</h1>
+                {this.state.message && <div className="alert alert-success">{this.state.message}</div>}
                 <div className="container">
                     <table className="table">
                         <thead>
@@ -25,6 +76,8 @@ class ListTodosComponent extends Component {
                                 <th>Description</th>
                                 <th>Is Completed?</th>
                                 <th>Target Date</th>
+                                <th>Update</th>
+                                <th>Delete</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -36,6 +89,8 @@ class ListTodosComponent extends Component {
                                             <td>{todo.description}</td>
                                             <td>{todo.done.toString()}</td>
                                             <td>{todo.targetDate.toString()}</td>
+                                            <td><button className="btn btn-warning" onClick={() => this.updateTodoClicked(todo.id)}>Update</button></td>
+                                            <td><button className="btn btn-danger" onClick={() => this.deleteTodoClicked(todo.id)}>Delete</button></td>
                                         </tr>
                                 )
                             }
